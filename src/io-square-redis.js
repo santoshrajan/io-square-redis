af0b70ade205f55e580cc3056fb319a9fd4ebfcf
Angad Gill <angadpalsingh92@gmail.com>
2016-10-25 16:14:27 +0530

    Initial working function

--- src/io-square-redis.js
+++ src/io-square-redis.js
@@ -1,1 +1,31 @@
-import IO from 'io-square'
+const IO = require('io-square')
+const db = require('redis')
+
+IO.redis = {
+  setClient (dbNum = 0, auth = false, password = null){
+    if (auth !== true) {
+      const c = db.createClient()
+      c.select(dbNum, db.print)
+      this.client = c
+    } else {
+      const c = db.createClient()
+      c.auth(password, db.print)
+      c.select(dbNum, db.print)
+      this.client = c
+    }
+  },
+  get (key) {
+    return new IO(cb => {
+      this.client.get(key, (err, reply)=>{
+       cb(reply)
+    })
+  })
+}
+}
+
+IO.redis.setClient(0, true, 'enterPasswordhere')
+
+IO.redis.get('job')
+  .then((val)=> {
+    console.log(val)
+  })
