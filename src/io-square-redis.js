9643cb733a7df7e9f95d7d67a867bdc9573059ac
Angad Gill <angadpalsingh92@gmail.com>
2016-10-25 18:35:49 +0530

    Add new methods
    Methods added :
    1. get
    2. set
    3. lpush
    4. rpush
    5. lpop
    6. rpop
    7. del
    8. exists
    9. hexists

--- src/io-square-redis.js
+++ src/io-square-redis.js
@@ -2,7 +2,7 @@ const IO = require('io-square')
 const db = require('redis')
 
 IO.redis = {
-  setClient (dbNum = 0, auth = false, password = null){
+  setClient (dbNum = 0, auth = false, password = null) {
     if (auth !== true) {
       const c = db.createClient()
       c.select(dbNum, db.print)
@@ -14,18 +14,51 @@ IO.redis = {
       this.client = c
     }
   },
+
   get (key) {
     return new IO(cb => this.client.get(key, cb))
-                  .map((e,v) => v)
+                 .map((e, v) => v)
   },
-  
+
+  set (key, val) {
+    return new IO(cb => this.client.set(key, val, cb))
+                 .map((e, confirmation) => confirmation === 'OK' ? val : e)
+  },
+
+  lpush (key, ...values) {
+    return new IO(cb => this.client.lpush(key, values, cb))
+                 .map((e, n) => values.length === n)
+  },
+
+  rpush (key, ...values) {
+    return new IO(cb => this.client.rpush(key, values, cb))
+                 .map((e, n) => values.length === n)
+  },
+
+  lpop (key) {
+    return new IO(cb => this.client.lpop(key, cb))
+               .map((e, v) => v)
+  },
+
+  rpop (key) {
+    return new IO(cb => this.client.rpop(key, cb))
+               .map((e, v) => v)
+  },
+
+  del (...keys) {
+    return new IO(cb => this.client.del(keys, cb))
+                               .map((e, n) => n)
+  },
+
+  exists (...keys) {
+    return new IO(cb => this.client.exists(keys, cb))
+                 .map((e, n) => keys.length === n) },
+
+  hexists (key, field) {
+    return new IO(cb => this.client.hexists(key, field, cb))
+                 .map((e, v) => v === 1)
+  }
+
 }
 
-IO.redis.setClient(0, true, 'TESTpassWord123')
-
-IO.redis.get('geekskool')
-  .error(err => console.log('error', err))
-  .then((a)=> {
-      console.log(a)
-      // console.log(val)
-  })
+module.exports = IO.redis
