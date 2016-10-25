e3e6274cb23dc9f4548c8de9c17db61954563d18
Angad Gill <angadpalsingh92@gmail.com>
2016-10-25 12:53:45 +0530

    Corrects files

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,18 +1,12 @@
 import gulp from 'gulp'
-import babelify from 'babelify'
-import browserify from 'browserify'
-import source from 'vinyl-source-stream'
+import babel from 'gulp-babel'
 import del from 'del'
+
+gulp.task('compile-lib', () =>
+  gulp.src('src/io-square-redis.js')
+  .pipe(babel())
+  .pipe(gulp.dest('lib')))
 
 gulp.task('clean', () => del(['lib']))
 
-gulp.task('build', () => {
-  browserify({entries: 'src/io-square-redis.js', standalone: 'IO'})
-    .transform(babelify)
-    .bundle()
-    .on('error', err => console.log(err))
-    .pipe(source('io-square-redis.js'))
-    .pipe(gulp.dest('lib'))
-})
-
-gulp.task('default', ['build'])
+gulp.task('default', ['compile-lib'])
