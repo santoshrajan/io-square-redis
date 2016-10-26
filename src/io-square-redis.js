5520754e2b8321150a5da750e7aca613084ff45e
Shruti Kale <shrutick05@gmail.com>
2016-10-26 09:01:34 +0530

    Wrote hget,hgetall,hdel functions

--- src/io-square-redis.js
+++ src/io-square-redis.js
@@ -2,7 +2,7 @@ const IO = require('io-square')
 const db = require('redis')
 
 IO.redis = {
-  setClient (dbNum = 0, auth = false, password = null){
+  setClient (dbNum = 0, auth = false, password = null) {
     if (auth !== true) {
       const c = db.createClient()
       c.select(dbNum, db.print)
@@ -14,18 +14,49 @@ IO.redis = {
       this.client = c
     }
   },
+
   get (key) {
     return new IO(cb => this.client.get(key, cb))
-                  .map((e,v) => v)
+      .map((e, v) => v)
   },
-  
+
+  hget (key, feild) {
+    return new IO(cb => this.client.hget(key, feild, cb))
+      .map((e, v) => v)
+  },
+  hgetall (key) {
+    return new IO(cb => this.client.hgetall(key, cb))
+      .map((e, v) => v)
+  },
+
+  hdel (key, feild) {
+    return new IO(cb => this.client.hdel(key, feild, cb))
+      .map((e, v) => v)
+  }
 }
 
-IO.redis.setClient(0, true, 'TESTpassWord123')
+// set password in redis.conf line 396
+IO.redis.setClient(0)
 
 IO.redis.get('geekskool')
   .error(err => console.log('error', err))
-  .then((a)=> {
-      console.log(a)
-      // console.log(val)
+  .then((a) => {
+    console.log(a)
+  })
+
+IO.redis.hget('myhash', 'field1')
+  .error(err => console.log('error', err))
+  .then((a) => {
+    console.log(a)
+  })
+IO.redis.hgetall('myhash')
+  .error(err => console.log('error', err))
+  .then((a) => {
+    console.log(a)
+  })
+
+IO.redis.hdel('myhash', 'field1')
+  .error(err => console.log('error', err))
+  .then((a) => {
+    console.log(a)
   })
